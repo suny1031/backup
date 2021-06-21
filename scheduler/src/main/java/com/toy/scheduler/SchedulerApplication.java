32d9afb1e27e4f1ab4758cdbafe2048da9ecff90
suny1031 <psuny1031@naver.com>
2021-06-21 19:05:09 +0900

    enum test 백업

--- scheduler/src/main/java/com/toy/scheduler/SchedulerApplication.java
+++ scheduler/src/main/java/com/toy/scheduler/SchedulerApplication.java
@@ -1,5 +1,8 @@
 package com.toy.scheduler;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.toy.scheduler.test.TestEnum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,8 +11,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class SchedulerApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JsonProcessingException {
         SpringApplication.run(SchedulerApplication.class, args);
+        for (TestEnum cate : TestEnum.values()) {
+            System.out.println(cate.getName()+","+cate.name());
+        }
     }
 
 }
---
+++ scheduler/src/main/java/com/toy/scheduler/test/TestVo.java
@@ -0,0 +1,20 @@
+package com.toy.scheduler.test;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class TestVo {
+
+    private String id;
+    private String password;
+    private String name;
+    private String date;
+
+
+
+}
