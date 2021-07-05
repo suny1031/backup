2abab0fc7e6a23e168402ce88e8fa7742b99282b
suny1031 <psuny1031@naver.com>
2021-07-05 18:47:00 +0900

    post 빡체네

--- test/src/main/java/com/toy/test/test/RestTemplate.java
+++ test/src/main/java/com/toy/test/test/RestTemplate.java
@@ -1,12 +1,27 @@
 package com.toy.test.test;
 
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/test")
 public class RestTemplate {
 
-    
+
+    @PostMapping("/post")
+    public void post(@RequestBody List<TestVo> testVos){
+        System.out.println("roQkrwlsp");
+
+        System.out.println(testVos.toString());
+
+    }
+
+
 
 
 
