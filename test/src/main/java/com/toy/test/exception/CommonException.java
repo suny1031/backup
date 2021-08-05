054d7d30561b5b818716e2bae80cc42061d546f2
suny1031 <psuny1031@naver.com>
2021-08-05 18:51:47 +0900

    전역 예외

---
+++ test/src/main/java/com/toy/test/exception/CommonException.java
@@ -0,0 +1,13 @@
+package com.toy.test.exception;
+
+
+
+public class CommonException extends  Exception{
+
+
+
+
+
+
+
+}
---
+++ test/src/main/java/com/toy/test/exception/ExceptionAdviceController.java
@@ -0,0 +1,23 @@
+package com.toy.test.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class ExceptionAdviceController {
+
+    @ExceptionHandler(CommonException.class)
+    @ResponseBody
+    public ResponseEntity<CommonException> ExceptionHandler(CommonException msg){
+        return new ResponseEntity<CommonException>(msg, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+
+
+}
