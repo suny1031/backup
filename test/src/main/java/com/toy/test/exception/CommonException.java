fc93784b04a1ba51b861aaad53a3c3be4280db21
suny1031 <psuny1031@naver.com>
2021-08-06 18:28:34 +0900

    예외 테스트

--- test/src/main/java/com/toy/test/exception/CommonException.java
+++ test/src/main/java/com/toy/test/exception/CommonException.java
@@ -5,6 +5,14 @@ package com.toy.test.exception;
 public class CommonException extends  Exception{
 
 
+    private String code;
+    private String msg;
+
+    public CommonException(){
+        this.code = code;
+        this.msg = msg;
+    }
+
 
 
 
